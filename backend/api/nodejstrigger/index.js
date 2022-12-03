85d5c56731160444c7d230c75d37c47044de3830
benjaminburton <benjaminlburton@gmail.com>
2022-12-03 13:55:59 -0500

    modifying api function for http request

--- backend/api/nodejstrigger/index.js
+++ backend/api/nodejstrigger/index.js
@@ -1,13 +1,21 @@
 const { CosmosClient } = require("@azure/cosmos");
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const client = new CosmosClient({ endpoint, key });
+  const client = new CosmosClient({ endpoint, key });
 
-    
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  const { databases } = await client.databases.createIfNotExists({
+    id: "azdatbases",
+  });
+  const { container } = await databases.createIfNotExists({ id: "" });
+
+  const { resources } = await container.items
+    .query("SELECT * from c")
+    .fetchAll();
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
