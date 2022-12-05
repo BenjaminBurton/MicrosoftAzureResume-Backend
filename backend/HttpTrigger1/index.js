7c83dd0f36ee939a9f574c53bf0e0041e2f69413
benjaminburton <benjaminlburton@gmail.com>
2022-12-05 11:45:29 -0500

    connected api successfully still modifying'

--- backend/HttpTrigger1/index.js
+++ backend/HttpTrigger1/index.js
@@ -3,15 +3,17 @@ const { CosmosClient } = require("@azure/cosmos");
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const name = req.query.name || (req.body && req.body.count);
+  const count = req.query.count || (req.body && req.body.count);
   const responseMessage = count
-    ? "Hello, " + count + ". This HTTP triggered function executed successfully."
+    ? "Hello, " +
+      count +
+      ". This HTTP triggered function executed successfully."
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-  if (name) {
+  if (count) {
     context.bindings.outputDocument = JSON.stringify({
       // create a random ID
-      id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+      id: 1,
       count: 0,
     });
   }
